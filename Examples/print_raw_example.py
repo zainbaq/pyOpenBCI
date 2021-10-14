4fc3115df5c369caec86b06c594cc30049b3bf5f
Zain Baquar <zainbaq>
2021-10-14 08:52:21 +0500

    change board from cyton to ganglion

--- Examples/print_raw_example.py
+++ Examples/print_raw_example.py
@@ -1,9 +1,9 @@
-from pyOpenBCI import OpenBCICyton
+from pyOpenBCI import OpenBCIGanglion
 
 def print_raw(sample):
     print(sample.channels_data)
 
 #Set (daisy = True) to stream 16 ch 
-board = OpenBCICyton(daisy = False)
+board = OpenBCIGanglion(mac=None) #Cyton(daisy = False)
 
 board.start_stream(print_raw)
